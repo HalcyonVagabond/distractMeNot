55826b848c7b72f880906ed1492a4ce623651950
Michael Clark <michaelclarknashville@protonmail.com>
2020-07-15 00:04:08 -0500

    suggested videos display none

--- main.js
+++ main.js
@@ -3,27 +3,20 @@ document.body.style.border = '10px solid blue';
 document.body.style.zIndex = '30002'
 document.getElementById('secondary').style.display = 'none'
 
-function groupedShit() {
+function removeSuggestions() {
     setTimeout(() => {
         document.getElementById('secondary').style.display = 'none'
         document.querySelector('.ytp-size-button, .ytp-button').click();
-        console.log(document.querySelector('.ytp-size-button'));
-        console.log('hello I am running')
     }, 2000);
 }
+//Removes Youtube Suggested videos, clicks on Theater Mode button
 
 function refreshPage() {
     setTimeout(() => {
         location.reload
         document.querySelector('.ytp-size-button, .ytp-button').click();
-        groupedShit()
+        removeSuggestions()
     }, 500)
-
 }
-
-// const spans = document.getElementsByClassName("css-901oao").style.display
-document.getElementsByClassName("css-901oao").style.display = 'none'
-// console.log(spans)
-console.log("yo homey")
 
 refreshPage()
