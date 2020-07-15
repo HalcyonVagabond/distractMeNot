8ba2d5ea7a1bbd51d2ea31ba1ccf269872fde9dc
Michael Clark <michaelclarknashville@protonmail.com>
2020-07-14 23:53:32 -0500

    suggested videos hidden on video page

--- main.js
+++ main.js
@@ -2,19 +2,28 @@
 document.body.style.border = '10px solid blue';
 document.body.style.zIndex = '30002'
 document.getElementById('secondary').style.display = 'none'
-setTimeout(()=>{
-    document.getElementById('secondary').style.display = 'none'
-},2000);
-setTimeout(()=>{
-    document.querySelector('.ytp-size-button, .ytp-button').click();
-    console.log(document.querySelector('.ytp-size-button'));
-    console.log('hello I am running')
-},2000);
 
-function refreshPage(){
-    setTimeout(()=>{
-        location.reload
-    },500)
+function groupedShit() {
+    setTimeout(() => {
+        document.getElementById('secondary').style.display = 'none'
+        document.querySelector('.ytp-size-button, .ytp-button').click();
+        console.log(document.querySelector('.ytp-size-button'));
+        console.log('hello I am running')
+    }, 2000);
 }
+
+function refreshPage() {
+    setTimeout(() => {
+        location.reload
+        document.querySelector('.ytp-size-button, .ytp-button').click();
+        groupedShit()
+    }, 500)
+
+}
+
+// const spans = document.getElementsByClassName("css-901oao").style.display
+document.getElementsByClassName("css-901oao").style.display = 'none'
+// console.log(spans)
+console.log("yo homey")
 
 refreshPage()
