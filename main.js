3e3b4633dab9b88780c0bd3281b168c1ebdbe73a
Jeremy Mattingly <jamattingly@live.com>
2020-07-15 00:45:25 -0500

    Three functions to remove homepage, suggestions, and comments. Each are being called within conditionals dependent upon window.location. Renamed in json to DistractMeNot with description.

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 
 document.body.style.border = '10px solid blue';
 document.body.style.zIndex = '30002'
-document.getElementById('secondary').style.display = 'none'
 
+function removeHomepage(){
+    // Removes the homepage section
+    document.getElementById('primary').style.display = 'none'
+}
 function removeSuggestions() {
+    //Removes Youtube Suggested videos, clicks on Theater Mode button
     setTimeout(() => {
+        document.getElementById('related').style.display = 'none'
         document.getElementById('secondary').style.display = 'none'
         document.querySelector('.ytp-size-button, .ytp-button').click();
     }, 2000);
 }
-//Removes Youtube Suggested videos, clicks on Theater Mode button
+function removeComments(){
+    // Removes the comment section
+    document.getElementById('comments').style.display = 'none'
+    document.getElementById('sections').style.display = 'none'
+}
 
 function refreshPage() {
-    setTimeout(() => {
-        location.reload
-        document.querySelector('.ytp-size-button, .ytp-button').click();
-        removeSuggestions()
-    }, 500)
+    if(window.location.href === 'https://www.youtube.com/'){
+        setTimeout(() => {
+            location.reload
+            removeHomepage()
+        }, 500)
+    }
+    if(window.location.href.includes('watch')){
+        setTimeout(() => {
+            location.reload
+            removeSuggestions()
+        }, 500)
+    }
+    if(window.location.href.includes('watch')){
+        setTimeout(() => {
+            location.reload
+            removeComments()
+        }, 500)
+    }
 }
 
 refreshPage()
